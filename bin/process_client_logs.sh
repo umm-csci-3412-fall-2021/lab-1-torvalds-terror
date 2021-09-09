f32f59105cb17fb3a6fc9f31ef5334024a10e8a5
Cole Maxwell <cole.n.maxwell99@gmail.com>
2021-09-09 11:22:24 -0500

    Add for loop to run awk on all files in given dir

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/bash
 
-awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" print "3. " groups[3] "\n"}' < r0_input.txt > r0_output.txt
+#Script takes a directory as input
+dir=$1
+
+#Enter given directory
+cd "$dir" || exit
+
+#Create a file for the failed logins
+touch failed_login_data.txt
+
+#Run awk regex check and print on all files in the given directory
+for FILE in *; do 
+    awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" > failed_login_data.txt
+done
+
+#Run awk regex check and print on all files in the given directory
+for FILE in *; do 
+    awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" > failed_login_data.txt
+done
