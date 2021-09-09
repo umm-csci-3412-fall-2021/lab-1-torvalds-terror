4d72c064d7c2b1c898ef3f3ef024b7175a198b96
zeus-ctrl <noah.constable@gmail.com>
2021-09-09 10:40:14 -0500

    Add regex for log processing

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/bash
+
+awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" print "3. " groups[3] "\n"}' < r0_input.txt > r0_output.txt
