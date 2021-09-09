90dda9d50e06d699a441dcc9aa71ffa76f0ec41f
Cole Maxwell <cole.n.maxwell99@gmail.com>
2021-09-09 11:48:55 -0500

    Change for loop to only accept .txt files

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -10,11 +10,11 @@ cd "$dir" || exit
 touch failed_login_data.txt
 
 #Run awk regex check and print on all files in the given directory
-for FILE in *; do 
+for FILE in *.txt; do 
     awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" > failed_login_data.txt
 done
 
 #Run awk regex check and print on all files in the given directory
-for FILE in *; do 
+for FILE in *.txt; do 
     awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" > failed_login_data.txt
 done
