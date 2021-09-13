4d1a4b85b0dafa65cb17fd157e7a2bf5a59fed52
zeus-ctrl <noah.constable@gmail.com>
2021-09-13 13:03:10 -0500

    Generalize regex for both cases in one line.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -11,10 +11,10 @@ touch failed_login_data.txt
 
 #Run awk regex check and print on all files in the given directory
 for FILE in *.txt; do 
-    awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" > failed_login_data.txt
+    awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for (|invalid user {1,})([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[3] groups[4] "\n"}' < "$FILE" > failed_login_data.txt
 done
 
-#Run awk regex check and print on all files in the given directory
-for FILE in *.txt; do 
-    awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" > failed_login_data.txt
-done
+# #Run awk regex check and print on all files in the given directory
+# for FILE in *.txt; do 
+#     awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" >> "failed_login_data.txt"
+# done
