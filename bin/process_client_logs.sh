12eb39fc5a9fbbffc808ed39a03b4da6e7f09d32
zeus-ctrl <noah.constable@gmail.com>
2021-09-14 11:22:27 -0500

    Fix regex and output correct sequence.
    Co-authored-by: Cole Maxwell <cole-maxwell1@users.noreply.github.com>

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,15 +6,9 @@ dir=$1
 #Enter given directory
 cd "$dir" || exit
 
-#Create a file for the failed logins
-touch failed_login_data.txt
-
 #Run awk regex check and print on all files in the given directory
-for FILE in *.txt; do 
-    awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for (|invalid user {1,})([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[3] groups[4] "\n"}' < "$FILE" > failed_login_data.txt
+for FILE in var/log/*; do 
+    echo "$FILE"
+    # awk 'match($0, /([a-zA-Z0-9]+)/, groups) {print $0 groups[1] " " groups[3] " " groups[4] "\n"}' < "$FILE" >> failed_login_data.txt
+    awk 'match($0, /([a-zA-Z]+)[ ]+([0-9]+) ([0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for (|invalid user {1,})([a-zA-Z0-9]+) from ([0-9.]+)/, groups) {print groups[1]  groups[2] " " groups[3] " " groups[5] " " groups[6]}' < "$FILE" >> failed_login_data.txt
 done
-
-# #Run awk regex check and print on all files in the given directory
-# for FILE in *.txt; do 
-#     awk 'match($0, /([a-zA-Z 0-9 0-9]+):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for ([a-zA-Z0-9]+) from ([0-9.0-9.0-9.0-9]+) port [0-9]+ ssh2/, groups) {print groups[1] groups[2] groups[3] "\n"}' < "$FILE" >> "failed_login_data.txt"
-# done
