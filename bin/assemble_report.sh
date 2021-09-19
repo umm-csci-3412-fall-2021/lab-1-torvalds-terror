3adb5d5ca2ceb9e10d78547a8cf76ab8c427842a
zeus-ctrl <noah.constable@gmail.com>
2021-09-19 16:35:01 -0500

    Complete assemble_reports.sh.

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/bash
+
+#Store the input directory
+DIR=$1
+
+#Concatenate the country, hour, and username distribution data into one "summary" file
+cat "$DIR"/country_dist.html "$DIR"/hours_dist.html "$DIR"/username_dist.html > "summary.html"
+
+#Execute the wrap_contents script and give it the summary file, the summary_plot header and footer, and write it in the input directory to failed_login_summary.html
+/bin/bash "$(dirname "$0")/wrap_contents.sh"  summary.html summary_plots "$DIR"/failed_login_summary.html
