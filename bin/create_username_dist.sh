79b72ba475fdb938711a291d70e28ee4fcf1a3b3
zeus-ctrl <noah.constable@gmail.com>
2021-09-20 19:14:53 -0500

    Finish create_username_dist script.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -3,16 +3,14 @@
 #Takes a directory as a command line input
 dir=$1
 
-#Gets the usernames from the failed_login_data.txt file and puts them all in a temporary file
-for FILE in "$dir"/*/var/log/*; do 
-    awk 'match($0, /[a-zA-Z]+ [0-9 ]+([a-zA-Z0-9]+)+[ 0-9.]+/, groups) {print groups[1]}' < failed_login_data.txt >> tmpUsername.txt
-done
+#Peek into the directory
+cd "$dir" || exit
 
-#Sorts the temp file of usernames for uniq
-sort "$dir"/tmpUsername.txt
+#Gets the usernames from the failed_login_data.txt file, sorts them, gets the frequency of their occurrences, and puts that in a temp file
+cat ./*/failed_login_data.txt | awk 'match($0, /[a-zA-Z]+ [0-9 ]+([a-zA-Z0-9]+)+[ 0-9.]+/, groups) {print groups[1]}' | sort | uniq -c | awk 'match($0, /([0-9]+) ([a-zA-Z0-9_]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' >> tmpAddRows.txt
 
-#Counts the occurences of each username and passes that to awk, which grabs the number of occurrences and username and prints them in a data.addRow line in another temp file
-uniq c tmpUsername.txt | awk 'match($0, /([0-9]+) ([a-zA-Z0-9_]+)/, groups) {print "data.addRow([\x27groups[2]\x27, groups[1]]);}' >> tmpAddRows.txt
+#Back it up a step
+cd .. || exit
 
 #Calls wrap contents on the temp file just created and passes the necessary html components
-/bin/bash "$(dirname "$0")/wrap_contents.sh" html_components/username_dist tmpAddRows.txt html_components/username_dist
+/bin/bash "$(dirname "$0")/wrap_contents.sh" "$dir"/tmpAddRows.txt username_dist "$dir"/username_dist.html
