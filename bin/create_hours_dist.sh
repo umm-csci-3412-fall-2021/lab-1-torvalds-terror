5cf6c4c931d1a6881e80c84d0bd608ea970d6a3f
zeus-ctrl <noah.constable@gmail.com>
2021-09-20 19:37:57 -0500

    Finish create_hours_dist script.

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/bash
+
+#Takes a directory as a command line input
+dir=$1
+
+#Peek into the directory
+cd "$dir" || exit
+
+#Gets the usernames from the failed_login_data.txt file, sorts them, gets the frequency of their occurrences, and puts that in a temp file
+cat ./*/failed_login_data.txt | awk 'match($0, /[a-zA-Z]+[ 0-9]+ ([0-9]+) [a-zA-Z0-9]+[ 0-9.]+/, groups) {print groups[1]}' | sort | uniq -c | awk 'match($0, /([0-9]+) ([a-zA-Z0-9_]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' >> tmpAddRows.txt
+
+#Back it up a step
+cd .. || exit
+
+#Calls wrap contents on the temp file just created and passes the necessary html components
+/bin/bash "$(dirname "$0")/wrap_contents.sh" "$dir"/tmpAddRows.txt hours_dist "$dir"/hours_dist.html
